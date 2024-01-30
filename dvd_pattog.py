e918f7bd630413371b2a27499c11c68eedf69d68
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2024-01-30 22:44:56 +0100

    kiírás kész

--- dvd_pattog.py
+++ dvd_pattog.py
@@ -1,7 +1,7 @@
 # FONTOS amíg még hátra vannak ebből a programból:
 # - Lecserélni valamire a dvd logót(ha akarjuk)
-# - idő számláló kíiratása ami nullázódik ha hozzáér a sarokhoz
 import pygame
+import time
 
 pygame.init()
 # képernyő hossza
@@ -12,14 +12,17 @@ screen_res = (width, height)
 
 pygame.display.set_caption("Pattogó dvd")
 screen = pygame.display.set_mode(screen_res)
+time_start = time.time()
 clock = pygame.time.Clock()
 # színek
 piros = (255, 0, 0)
 fekete = (0, 0, 0)
+fehér = (255, 255, 255)
 
-# (PRÓBA) labda
-# labda = pygame.draw.circle(surface=screen, color=piros, center=[100, 100], radius=40)
-
+#kiírás
+game_font = pygame.font.SysFont('Verdana', 60)
+text_surf = game_font.render('GAME', True, fehér)
+text_rect = text_surf.get_rect(center=(width / 2, height / 2))
 #dvd kép animáció összerakása
 dvdkép1 = pygame.image.load("dvd.png")
 dvdkép2= pygame.image.load("dvd2.png")
@@ -35,6 +38,7 @@ dvd_rect = dvd[dvd_index].get_rect(midleft=(240, 160))
 # milyen gyors
 # gyorsaság = [X tengelyen, Y tengelyen]
 gyorsaság = [5.3, 6.2]
+score = 0
 # game loop
 while True:
     # event loop
@@ -42,6 +46,14 @@ while True:
         # check if a user wants to exit the game or not
         if event.type == pygame.QUIT:
             exit()
+
+    #idő számlálás
+    game_time = str(int(time.time() - time_start))
+    time_surf = game_font.render('Idő: ' + game_time, True, fehér)
+    time_rect = time_surf.get_rect(topleft=(10,10))
+
+    score_surf = game_font.render('Falhoz érések száma: ' + str(score), True, fehér)
+    score_rect = score_surf.get_rect(topright=(width - 10, 10))
 
     # háttér kitoltése
     screen.fill(fekete)
@@ -54,13 +66,17 @@ while True:
     if dvd_rect.left <= 0 or dvd_rect.right >= width:
         gyorsaság[0] = -gyorsaság[0]
         dvd_index+=1
+        score+=1
     if dvd_rect.top <= 0 or dvd_rect.bottom >= height:
         gyorsaság[1] = -gyorsaság[1]
         dvd_index+=1
+        score+=1
 
     if dvd_index>len(dvd)-1:
         dvd_index=0
     
+    screen.blit(time_surf, time_rect)
+    screen.blit(score_surf,score_rect)
     screen.blit(dvd[dvd_index], dvd_rect)
     # frissiteni a képernyőt
     pygame.display.update()
