0d9d1b1524ad93aa2421a9a27973641a72553a3e
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2024-01-31 19:10:22 +0100

    denevéres kész

--- denevér.py
+++ denevér.py
@@ -1,5 +1,3 @@
-# FONTOS amíg még hátra vannak ebből a programból:
-# - Lecserélni valamire a dvd logót(ha akarjuk)
 import pygame
 import time
 
@@ -10,30 +8,27 @@ width = 1600
 height = 1080
 screen_res = (width, height)
 
-pygame.display.set_caption("Pattogó dvd")
+pygame.display.set_caption("Repülő denevér")
 screen = pygame.display.set_mode(screen_res)
 time_start = time.time()
 clock = pygame.time.Clock()
 # színek
 piros = (255, 0, 0)
-fekete = (80, 10, 20)
+Háttérkép = pygame.image.load("fák.webp")
+Háttér=pygame.transform.scale(Háttérkép,(1600,1080))
 fehér = (255, 255, 255)
 
 # kiírás
 game_font = pygame.font.SysFont("Verdana", 60)
 text_surf = game_font.render("GAME", True, fehér)
 text_rect = text_surf.get_rect(center=(width / 2, height / 2))
-# dvd kép animáció összerakása
-batkép1 = pygame.image.load("denevér1.png")
-batkép2 = pygame.image.load("denevér2.png")
-batkép3 = pygame.image.load("denevér3.png")
-batkép4 = pygame.image.load("denevér4.png")
-bat1 = pygame.transform.scale(batkép1, (240, 160)).convert_alpha()
-bat2 = pygame.transform.scale(batkép2, (240, 160)).convert_alpha()
-bat3 = pygame.transform.scale(batkép3, (240, 160)).convert_alpha()
-bat4 = pygame.transform.scale(batkép4, (240, 160)).convert_alpha()
-bat5 = pygame.transform.scale(batkép3, (240, 160)).convert_alpha()
-bat6 = pygame.transform.scale(batkép2, (240, 160)).convert_alpha()
+# denevér kép animáció összerakása
+bat1 = pygame.image.load("denevér1.png").convert_alpha()
+bat2 = pygame.image.load("denevér2.png").convert_alpha()
+bat3 = pygame.image.load("denevér3.png").convert_alpha()
+bat4 = pygame.image.load("denevér4.png").convert_alpha()
+bat5 = pygame.image.load("denevér3.png").convert_alpha()
+bat6 = pygame.image.load("denevér2.png").convert_alpha()
 bat = [bat1, bat2, bat3, bat4, bat5, bat6]
 bat_index = 0
 bat_rect = bat[bat_index].get_rect(midleft=(240, 160))
@@ -59,7 +54,7 @@ while True:
     score_rect = score_surf.get_rect(topright=(width - 10, 10))
 
     # háttér kitoltése
-    screen.fill(fekete)
+    screen.blit(Háttér,(0,0))
     # bat animáció
     számláló += 1
     if számláló % 5 == 0:
@@ -67,10 +62,10 @@ while True:
     if bat_index > len(bat) - 1:
         bat_index = 0
 
-    # dvd mozog
+    # denevér mozog
     bat_rect = bat_rect.move(gyorsaság)
     # ezen a mentén fog a falunk mozogni
-    # ha a dvd kimely a képből jöjjön vissza
+    # ha a denevér kimely a képből jöjjön vissza
     if bat_rect.left <= 0 or bat_rect.right >= width:
         gyorsaság[0] = -gyorsaság[0]
         score += 1
