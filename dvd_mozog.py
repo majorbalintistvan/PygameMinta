8b9c4e578c256fc58fcba3a36ce81fc588321a95
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2024-01-31 18:38:56 +0100

    denevér

--- dvd_mozog.py
+++ dvd_mozog.py
@@ -16,25 +16,28 @@ time_start = time.time()
 clock = pygame.time.Clock()
 # színek
 piros = (255, 0, 0)
-fekete = (0, 0, 0)
+fekete = (80, 10, 20)
 fehér = (255, 255, 255)
 
-#kiírás
-game_font = pygame.font.SysFont('Verdana', 60)
-text_surf = game_font.render('GAME', True, fehér)
+# kiírás
+game_font = pygame.font.SysFont("Verdana", 60)
+text_surf = game_font.render("GAME", True, fehér)
 text_rect = text_surf.get_rect(center=(width / 2, height / 2))
-#dvd kép animáció összerakása
-dvdkép1 = pygame.image.load("dvd.png")
-dvdkép2= pygame.image.load("dvd2.png")
-dvdkép3= pygame.image.load("dvd3.png")
-dvdkép4= pygame.image.load("dvd4.png")
-dvd1 = pygame.transform.scale(dvdkép1, (240, 160)).convert_alpha()
-dvd2 = pygame.transform.scale(dvdkép2, (240, 160)).convert_alpha()
-dvd3 = pygame.transform.scale(dvdkép3, (240, 160)).convert_alpha()
-dvd4 = pygame.transform.scale(dvdkép4, (240, 160)).convert_alpha()
-dvd=[dvd1,dvd2,dvd3,dvd4]
-dvd_index=0
-dvd_rect = dvd[dvd_index].get_rect(midleft=(240, 160))
+# dvd kép animáció összerakása
+batkép1 = pygame.image.load("denevér1.png")
+batkép2 = pygame.image.load("denevér2.png")
+batkép3 = pygame.image.load("denevér3.png")
+batkép4 = pygame.image.load("denevér4.png")
+bat1 = pygame.transform.scale(batkép1, (240, 160)).convert_alpha()
+bat2 = pygame.transform.scale(batkép2, (240, 160)).convert_alpha()
+bat3 = pygame.transform.scale(batkép3, (240, 160)).convert_alpha()
+bat4 = pygame.transform.scale(batkép4, (240, 160)).convert_alpha()
+bat5 = pygame.transform.scale(batkép3, (240, 160)).convert_alpha()
+bat6 = pygame.transform.scale(batkép2, (240, 160)).convert_alpha()
+bat = [bat1, bat2, bat3, bat4, bat5, bat6]
+bat_index = 0
+bat_rect = bat[bat_index].get_rect(midleft=(240, 160))
+számláló = 0
 # milyen gyors
 # gyorsaság = [X tengelyen, Y tengelyen]
 gyorsaság = [5.3, 6.2]
@@ -47,37 +50,37 @@ while True:
         if event.type == pygame.QUIT:
             exit()
 
-    #idő számlálás
+    # idő számlálás
     game_time = str(int(time.time() - time_start))
-    time_surf = game_font.render('Idő: ' + game_time, True, fehér)
-    time_rect = time_surf.get_rect(topleft=(10,10))
+    time_surf = game_font.render("Idő: " + game_time, True, fehér)
+    time_rect = time_surf.get_rect(topleft=(10, 10))
 
-    score_surf = game_font.render('Falhoz érések száma: ' + str(score), True, fehér)
+    score_surf = game_font.render("Falhoz érések száma: " + str(score), True, fehér)
     score_rect = score_surf.get_rect(topright=(width - 10, 10))
 
     # háttér kitoltése
     screen.fill(fekete)
-    #dvd animáció
+    # bat animáció
+    számláló += 1
+    if számláló % 5 == 0:
+        bat_index += 1
+    if bat_index > len(bat) - 1:
+        bat_index = 0
+
     # dvd mozog
-    dvd_rect = dvd_rect.move(gyorsaság)
-    # mostmár a dvd közepe (101,101)
+    bat_rect = bat_rect.move(gyorsaság)
     # ezen a mentén fog a falunk mozogni
     # ha a dvd kimely a képből jöjjön vissza
-    if dvd_rect.left <= 0 or dvd_rect.right >= width:
+    if bat_rect.left <= 0 or bat_rect.right >= width:
         gyorsaság[0] = -gyorsaság[0]
-        dvd_index+=1
-        score+=1
-    if dvd_rect.top <= 0 or dvd_rect.bottom >= height:
+        score += 1
+    if bat_rect.top <= 0 or bat_rect.bottom >= height:
         gyorsaság[1] = -gyorsaság[1]
-        dvd_index+=1
-        score+=1
+        score += 1
 
-    if dvd_index>len(dvd)-1:
-        dvd_index=0
-    
     screen.blit(time_surf, time_rect)
-    screen.blit(score_surf,score_rect)
-    screen.blit(dvd[dvd_index], dvd_rect)
+    screen.blit(score_surf, score_rect)
+    screen.blit(bat[bat_index], bat_rect)
     # frissiteni a képernyőt
     pygame.display.update()
     clock.tick(60)
